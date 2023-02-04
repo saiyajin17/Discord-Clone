41d8974c5c83cd9cb0945f1dce6c114579d5858a
saiyajin17 <106419562+saiyajin17@users.noreply.github.com>
2023-02-04 21:11:51 +0530

    CLone Created

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        
+        
+      },
+      colors:{
+        bgBlue:"#404EED",
+        bgWhiteSmoke:"#F6F6F6",
+        blue:"#5865F2",
+        blueDark:"#5363F2",
+        gray:"#23272A",
+        paraColor:"#2A3175",
+      }
+    },
+  },
+  plugins: [],
+}
